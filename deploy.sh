9250b57b8668d5eb9e09030794fee75c5091750e
r3dsp1 <mxkeith1904@gmail.com>
2018-06-08 01:35:26 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+cd
+
+rm -f -R ethos_monitor
+
+git clone https://github.com/r3dsp1/ethos_monitor
+
+chmod -R 775 ethos_monitor/
+
+rm -f custom.sh
+
+cp ~/ethos_monitor/custom.sh ~/custom.sh
+
+chmod -R 755 custom.sh
+
+echo "1" > /opt/ethos/etc/autorebooted.file
+
+sudo timedatectl set-timezone Asia/Hong_Kong
+
+sudo reboot
